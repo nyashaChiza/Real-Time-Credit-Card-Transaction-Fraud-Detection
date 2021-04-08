1937cd0773de758814d7f8bda16c7b7d908be293
nyashaChiza <nchizampeni@gmail.com>
2021-04-08 16:10:58 +0200

    changes

--- API/api.py
+++ API/api.py
@@ -51,5 +51,29 @@ def load_user_data(user_id):
     data =  Data.query.filter_by(Client_id= user_id).all()
     return data
 #-----------------------------------------------------------------------
-            
-
+def bar_graph_loader(user_id):
+    fraud = Data.query.filter_by(Client_id= user_id, label=0).all()
+    clean = Data.query.filter_by(Client_id= user_id, label=1).all()
+    return [len(fraud), len(clean)]
+#-----------------------------------------------------------------------
+def unique(list1):
+    list_set = set(list1)
+    unique_list = (list(list_set))
+    return unique_list
+#-----------------------------------------------------------------------
+def grouped_bar_graph_loader(user_id):
+    
+    cities= []
+    fraud_t = []
+    clean_t = []
+    data = Data.query.filter_by(Client_id= user_id, ).all()
+    for x in data:
+        cities.append(x.location)
+    cities = unique(cities)
+    for x in cities:
+        clean = Data.query.filter_by(Client_id= user_id, location= x, label=0 ).all()
+        fraud = Data.query.filter_by(Client_id= user_id, location= x, label=1 ).all()
+        fraud_t.append(len(fraud))
+        clean_t.append(len(clean))
+    return [cities, fraud_t, clean_t]
+#-----------------------------------------------------------------------
--- API/test.py
+++ API/test.py
@@ -1,8 +1,24 @@
 from models import *
 
-def load_user_data(user_id):
-    return Data.query.filter_by(Client_id = user_id ).all()
+def unique(list1):
+    list_set = set(list1)
+    unique_list = (list(list_set))
+    return unique_list
 
-data = load_user_data(1) 
-#for x in data:
-print(len(data))
+def grouped_bar_graph_loader(user_id):
+    
+    cities= []
+    fraud_t = []
+    clean_t = []
+    data = Data.query.filter_by(Client_id= user_id, ).all()
+    for x in data:
+        cities.append(x.location)
+    cities = unique(cities)
+    for x in cities:
+        clean = Data.query.filter_by(Client_id= user_id, location= x, label=1 ).all()
+        fraud = Data.query.filter_by(Client_id= user_id, location= x, label=0 ).all()
+        fraud_t.append(len(fraud))
+        clean_t.append(len(clean))
+    return [cities,fraud_t,clean_t]
+    
+print(grouped_bar_graph_loader(1))
